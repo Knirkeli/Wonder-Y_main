a4de5664728578263566766b4de18dbd7594735b
Eirik Kirkeli <knirkeli88@msn.com>
2023-11-17 21:48:49 +0100

    cleenup

--- js/nav.mjs
+++ js/nav.mjs
@@ -1,24 +1,3 @@
-// // run code after dom content has loaded
-// document.addEventListener('DOMContentLoaded', (event) => {
-//     // Get all the links in the navigation menu
-//     const navLinks = document.querySelectorAll('.nav-link');
-
-//     // Loop through each link
-//     for (var i =0; i < navLinks.length; i++) {
-//         //check if the link's href is the same as the current page
-//         var linkPath = navLinks[i].getAttribute('href');
-//         if (linkPath !== '/') {
-//             linkPath = '/' + linkPath;
-//         }
-//         if (linkPath == window.location.pathname) {
-//             //add the active class to the link
-//             navLinks[i].classList.add('active');
-//         } else {
-//             //remove the active class from the link
-//             navLinks[i].classList.remove('active');
-//         }
-//     }
-// });
 /**
  * This event listener waits for the DOM content to be fully loaded.
  * Once loaded, it gets all the links in the navigation menu and checks if their href is the same as the current page's pathname.
